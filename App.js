1623a36246bbc303d037bdfac1152aea9889df1a
Nicky Huynh <98395576+NguyenNhatHuynh@users.noreply.github.com>
2024-04-27 15:42:59 +0700

    SetuP

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {Alert, Modal, StyleSheet, Text, Pressable, View,TextInput} from 'react-native';
 
-export default function App() {
+import Bai1 from './Tuan03/Week3-Ex1'
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {styles.container}>
+      <Week3-Ex1/>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  
   },
 });
+
+export default App;
